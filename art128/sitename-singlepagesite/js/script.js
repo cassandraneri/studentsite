a1a326ab4c3866e3c1f28681dceb1f276a4164dd
cassandraneri <neric@hawaii.edu>
2022-03-03 10:19:41 -1000

    added z-index to header

--- art128/sitename-singlepagesite/js/script.js
+++ art128/sitename-singlepagesite/js/script.js
@@ -16,9 +16,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
     //STICKY NAV LINKS TOGGLE MENU NAV
-    var stickynavlinks = document.querySelectorAll(".sticky nav a");
-    var stickyheader = document.querySelector(".sticky");
-    var j;
+    const stickynavlinks = document.querySelectorAll(".sticky nav a");
+    const stickyheader = document.querySelector(".sticky");
+    let j;
     for (j = 0; j < stickynavlinks.length; j++) {
         stickynavlinks[j].onclick = function () {
             stickyheader.setAttribute('data-navstate', 'closed');
