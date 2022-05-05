8f68e7832777b2d6b38e6651249dd4269f38e086
cassandraneri <neric@hawaii.edu>
2022-05-05 12:01:33 -1000

    updated hawaiicatcafefinal

--- art128/hawaiicatcafefinal/js/script.js
+++ art128/hawaiicatcafefinal/js/script.js
@@ -27,3 +27,29 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+  slidesPerView: 1,
+          spaceBetween: 30,
+          loop: true,
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
