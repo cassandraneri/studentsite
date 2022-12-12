e0254c6edce6dc74485767ae419967c638b05389
cassandraneri <neric@hawaii.edu>
2022-12-11 15:47:43 -1000

    added coded draft links to art258 page

---
+++ art258/happyworms-v2/js/index.js
@@ -0,0 +1,7 @@
+var animation1 = lottie.loadAnimation({
+    container: document.getElementById('bm1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'img/FruitVeggies.json'
+  })
